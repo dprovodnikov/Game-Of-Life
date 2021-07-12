2f6ef6dbda29f4ca84662d4d03d8908bd639a3b0
Denys <dp.wireden@gmail.com>
2021-07-12 20:16:51 +0300

    Update gameOfLife tests to use enum values instead of raw numbers

--- src/domain/__tests__/gameOfLife.spec.ts
+++ src/domain/__tests__/gameOfLife.spec.ts
@@ -5,31 +5,24 @@ describe('GameOfLife', () => {
   it('initializes to a completely dead grid by default', () => {
     const game = createGameOfLife(3, 3);
     expect(game.getGrid()).toEqual([
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
+      [Cell.Dead, Cell.Dead, Cell.Dead],
+      [Cell.Dead, Cell.Dead, Cell.Dead],
+      [Cell.Dead, Cell.Dead, Cell.Dead],
     ]);
   });
 
   it('allows to populate each cell', () => {
     const game = createGameOfLife(3, 3);
+    const desiredGrid: Grid = [
+      [Cell.Dead, Cell.Dead, Cell.Alive],
+      [Cell.Dead, Cell.Dead, Cell.Dead],
+      [Cell.Alive, Cell.Dead, Cell.Dead],
+    ];
 
     game.populateEachCell((rowIndex, cellIndex) => {
-      if (rowIndex === 2 && cellIndex === 0) {
-        return Cell.Alive;
-      }
-
-      if (rowIndex === 0 && cellIndex === 2) {
-        return Cell.Alive;
-      }
-
-      return Cell.Dead;
+      return desiredGrid[rowIndex][cellIndex];
     });
 
-    expect(game.getGrid()).toEqual([
-      [0, 0, 1],
-      [0, 0, 0],
-      [1, 0, 0],
-    ]);
+    expect(game.getGrid()).toEqual(desiredGrid);
   });
 });
